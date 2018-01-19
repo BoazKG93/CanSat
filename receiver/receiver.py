dae79a7d723fabbf9bf1bf18727f4cfcc1db9899
Boaz Keren Gil <boazkg@icloud.com>
2018-01-19 18:03:46 +0100

    Full connectivity

--- receiver/receiver.py
+++ receiver/receiver.py
@@ -1,11 +1,81 @@
-from functions import *
+def sendToArduino(sendStr):
+  ser.write(sendStr)
+
+
+def recvFromArduino():
+  global startMarker, endMarker
+  
+  ck = ""
+  x = "z" # any value that is not an end- or startMarker
+  byteCount = -1 # to allow for the fact that the last increment will be one too many
+  
+  # wait for the start character
+  while  ord(x) != startMarker: 
+    x = ser.read()
+  
+  # save data until the end marker is found
+  while ord(x) != endMarker:
+    if ord(x) != startMarker:
+      ck = ck + x 
+      byteCount += 1
+    x = ser.read()
+  
+  return(ck)
+
+
+def waitForArduino():
+
+   # wait until the Arduino sends 'Arduino Ready' - allows time for Arduino reset
+   # it also ensures that any bytes left over from a previous message are discarded
+   
+    global startMarker, endMarker
+    
+    msg = ""
+    while msg.find("Satallite is ready") == -1:
+
+      while ser.inWaiting() == 0:
+        pass
+        
+      msg = recvFromArduino()
+
+      print msg
+      
+
+def communicate(td):
+  numLoops = len(td)
+  waitingForReply = False
+
+  n = 0
+  while n < numLoops:
+
+    teststr = td[n]
+
+    if waitingForReply == False:
+      sendToArduino(teststr)
+      print "Sent from PC -- "+str(teststr)
+      waitingForReply = True
+
+    if waitingForReply == True:
+
+      while ser.inWaiting() == 0:
+        pass
+        
+      dataRecvd = recvFromArduino()
+      print "Reply Received: " + dataRecvd
+      n += 1
+      waitingForReply = False
+
+      print "==========="
+
+    time.sleep(5)
+
 import serial
 import time
 
 if __name__ == "__main__":
   # NOTE the user must ensure that the serial port and baudrate are correct
-  serPort = raw_input("Please enter the Serial port: ")
-  baudRate = input("Please enter the Baudrate: ")
+  serPort = "/dev/cu.usbserial-DN02MUUN" #raw_input("Please enter the Serial port: ")
+  baudRate = 9600 #input("Please enter the Baudrate: ")
   ser = serial.Serial(serPort, baudRate)
   print "Serial port " + serPort + " opened  Baudrate " + str(baudRate)
 
@@ -13,26 +83,25 @@ if __name__ == "__main__":
   startMarker = 60
   endMarker = 62
 
-  sendData = []
-
-  input("Press Enter to turn the satallite on")
-
-  sendData.append(1) #1 is online signal
-
-  communicate(sendData)
-
-  #Receive data for 5 minutes
-  timeout = time.time() + 60*5
-  while True:
-      time.sleep(0.5)
-      timer = 0
-      if timer == 5 or time.time() > timeout:
-          break
-      data = recvFromArduino()
-      print data
-      #TODO: Save to CSV
-      time.sleep(0.5)
-      timer = timer - 1
+  ser.flushInput()
+  ser.flushOutput()
+  with open('data.csv', 'w+') as the_file:
+    the_file.write("Timestamp (ms),innerAnalogTemp,outerAnalogTemp,pressure,altitude,AccX,AccY,AccZ,innerDigitalTemp\n")
+    timeout = time.time() + 60
+    while True:
+#        time.sleep(0.25)
+        timer = 0
+        if timer == 5 or time.time() > timeout:
+            break
+        ser.flushInput()
+        ser.flushOutput()
+        data = recvFromArduino()
+        print data
+        the_file.write(data)
+        the_file.write("\n")
+        timer = timer - 1
+#        time.sleep(0.25)
+  
 
   ser.close
 
