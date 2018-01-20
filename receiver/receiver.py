c0b3c22bf81db1fef6f9e99987a00fb9df0dc4d5
Boaz Keren Gil <boazkg@icloud.com>
2018-01-20 16:37:07 +0100

    Full connectivity

--- receiver/receiver.py
+++ receiver/receiver.py
@@ -1,5 +1,5 @@
 def sendToArduino(sendStr):
-  ser.write(sendStr)
+  ser.write(sendStr.encode())
 
 
 def recvFromArduino():
@@ -16,7 +16,7 @@ def recvFromArduino():
   # save data until the end marker is found
   while ord(x) != endMarker:
     if ord(x) != startMarker:
-      ck = ck + x 
+      ck = ck + x.decode('utf-8')
       byteCount += 1
     x = ser.read()
   
@@ -38,7 +38,7 @@ def waitForArduino():
         
       msg = recvFromArduino()
 
-      print msg
+      print(msg)
       
 
 def communicate(td):
@@ -52,7 +52,7 @@ def communicate(td):
 
     if waitingForReply == False:
       sendToArduino(teststr)
-      print "Sent from PC -- "+str(teststr)
+      print("Sent from PC -- ",str(teststr))
       waitingForReply = True
 
     if waitingForReply == True:
@@ -61,46 +61,61 @@ def communicate(td):
         pass
         
       dataRecvd = recvFromArduino()
-      print "Reply Received: " + dataRecvd
+      print("Reply Received: ",dataRecvd)
       n += 1
       waitingForReply = False
 
-      print "==========="
+      print("===========")
 
     time.sleep(5)
+  # Define callback.
+  def my_data_received_callback(xbee_message):
+      address = xbee_message.remote_device.get_64bit_addr()
+      data = xbee_message.data.decode("utf8")
+      print("Received data from %s: %s" % (address, data))
 
 import serial
 import time
-
 if __name__ == "__main__":
   # NOTE the user must ensure that the serial port and baudrate are correct
   serPort = "/dev/cu.usbserial-DN02MUUN" #raw_input("Please enter the Serial port: ")
   baudRate = 9600 #input("Please enter the Baudrate: ")
   ser = serial.Serial(serPort, baudRate)
-  print "Serial port " + serPort + " opened  Baudrate " + str(baudRate)
-
+  print("Serial port ",serPort," opened  Baudrate ",str(baudRate))
 
   startMarker = 60
   endMarker = 62
-
-  ser.flushInput()
-  ser.flushOutput()
-  with open('data.csv', 'w+') as the_file:
-    the_file.write("Timestamp (ms),innerAnalogTemp,outerAnalogTemp,pressure,altitude,AccX,AccY,AccZ,innerDigitalTemp\n")
-    timeout = time.time() + 60
-    while True:
-#        time.sleep(0.25)
-        timer = 0
-        if timer == 5 or time.time() > timeout:
-            break
-        ser.flushInput()
-        ser.flushOutput()
-        data = recvFromArduino()
-        print data
-        the_file.write(data)
-        the_file.write("\n")
-        timer = timer - 1
-#        time.sleep(0.25)
+  input("Turn on sensors?")
+  #TODO: Implement a send/rec signal to turn sat online
+  sendToArduino("1")
+  onlineResponse = recvFromArduino()
+  if(onlineResponse == "Satallite is ready"):
+    print(onlineResponse)
+    print(recvFromArduino())
+    print(recvFromArduino())
+    print("==================")
+    input("Launch it?")
+    sendToArduino("1")
+    print(recvFromArduino())
+    print("==================")
+    ser.flushInput()
+    ser.flushOutput()
+    with open('data.csv', 'w+') as the_file:
+      the_file.write("Timestamp (ms),innerAnalogTemp,outerAnalogTemp,pressure,altitude,AccX,AccY,AccZ,innerDigitalTemp\n")
+      timeout = time.time() + 60
+      while True:
+  #        time.sleep(0.25)
+          timer = 0
+          if timer == 5 or time.time() > timeout:
+              break
+          ser.flushInput()
+          ser.flushOutput()
+          data = recvFromArduino()
+          print(data)
+          the_file.write(data)
+          the_file.write("\n")
+          timer = timer - 1
+  #        time.sleep(0.25)
   
 
   ser.close
